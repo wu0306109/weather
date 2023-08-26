f4d9670a7c42d040b7f12234b52db33791a62549
Young Wu <young.wu@worldedtech.org>
2023-08-26 22:43:53 +0800

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Young on 2023/8/26.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
